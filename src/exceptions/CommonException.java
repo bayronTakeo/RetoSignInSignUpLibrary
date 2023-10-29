fdaab90e92721b215f57ed62ff9089de2b51afc6
bayro <bayrontakeoch21@gmail.com>
2023-10-29 23:31:55 +0100

    Añado nueva excepcion comun

---
+++ src/exceptions/CommonException.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author bayro
+ */
+public class CommonException extends Exception {
+
+    public CommonException(String type) {
+        super(getMessageForType(type));
+    }
+
+    private static String getMessageForType(String type) {
+        switch (type) {
+            case "empty":
+                return "This field can´t be blank";
+            case "spaces":
+                return "This field can´t contains blank spaces";
+            case "symbols":
+                return "This field cannot contain special characters";
+            case "numbers":
+                return "This field cannot contain numbers";
+            case "letters":
+                return "This field cannot contain letters";
+            default:
+                return "Something has gone wrong";
+        }
+    }
+}
